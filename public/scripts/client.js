4553517949062fcfc8cbbfc3a04058a60042df73
sharaseaman <shara.seaman@gmail.com>
2017-09-24 20:38:48 -0500

    pic works

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,22 @@
 console.log('script sourced');
+
+var app = angular.module('app', []);
+var show = true;
+
+
+app.controller('GalleryController', function(){
+  var vm = this;
+  vm.cast = [];
+  var star = {
+    name: "Jacey",
+    role: "The niece",
+    imageUrl: 'img/jacey_pose.jpg',
+    view: true
+  };
+  vm.cast.push ( star );
+
+
+  vm.togglePic = function( e ){
+    vm.cast[ e ].view = !vm.cast[ e ].view;
+  };
+});
