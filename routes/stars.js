e1b573b779a2a3e64cf3df93ca752f73ba93c190
sharaseaman <shara.seaman@gmail.com>
2017-09-28 12:47:46 -0500

    server side refactor

--- routes/stars.js
+++ routes/stars.js
@@ -4,21 +4,21 @@ var router = express.Router();
 var cast = [];
 
 //put star objects here with push
-cast.push( {url: 'http://10.100.100.253:5000/img/jacey_pose.jpg' ,name: 'Jacey', role: 'The Niece'});
-cast.push( {url: 'http://10.100.100.253:5000/img/jacey_pose.jpg' ,name: 'Jacey', role: 'The Niece'});
-cast.push( {url: 'http://10.100.100.253:5000/img/jacey_pose.jpg' ,name: 'Jacey', role: 'The Niece'});
-cast.push( {url: 'http://10.100.100.253:5000/img/jacey_pose.jpg' ,name: 'Jacey', role: 'The Niece'});
-cast.push( {url: 'http://10.100.100.253:5000/img/jacey_pose.jpg' ,name: 'Jacey', role: 'The Niece'});
-cast.push( {url: 'http://10.100.100.253:5000/img/jacey_pose.jpg' ,name: 'Jacey', role: 'The Niece'});
+cast.push( {url: 'http://localhost:5000/img/jacey_pose.jpg' ,name: 'Jacey', role: 'The Niece'});
+cast.push( {url: 'http://localhost:5000/img/jacey_pose.jpg' ,name: 'Jacey', role: 'The Niece'});
+cast.push( {url: 'http://localhost:5000/img/jacey_pose.jpg' ,name: 'Jacey', role: 'The Niece'});
+cast.push( {url: 'http://localhost:5000/img/jacey_pose.jpg' ,name: 'Jacey', role: 'The Niece'});
+cast.push( {url: 'http://localhost:5000/img/jacey_pose.jpg' ,name: 'Jacey', role: 'The Niece'});
+cast.push( {url: 'http://localhost:5000/img/jacey_pose.jpg' ,name: 'Jacey', role: 'The Niece'});
 
 
 router.get( '/', function(req,res){
     res.send(cast);
 });
 
-// router.put( '/', function( req, res) {
-//     stars[req.body.index ].touches++;
-//     res.send( 200 );
-// });
+router.put( '/', function( req, res) {
+    stars[req.body.index ].touches++;
+    res.send( 200 );
+});
 
 module.exports = router;
