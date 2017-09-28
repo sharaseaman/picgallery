51beee69e6c665d1c379d13a141cdef6aa10fa6f
sharaseaman <shara.seaman@gmail.com>
2017-09-28 12:48:39 -0500

    added env PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 //route vars
 var indexRouter = require('./routes/index');
